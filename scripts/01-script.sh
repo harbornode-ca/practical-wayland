1a91627f65668b3d2a03ef59e0e6223de9c1a16b
Kevrevrun <kevin@harbornode.ca>
2026-09-15 20:05:14 -0400

    refactor: increase sleep durations and refine output messages in setup script

--- scripts/01-script.sh
+++ scripts/01-script.sh
@@ -4,12 +4,12 @@
 #repetative loading of variables for every script.
 echo
 echo "Confirming directory structure"
-sleep 0.5
+sleep 1
 for folder in "cfg" "status" "scripts" "tmp" "logs"; do
     if [ ! -d "/opt/kevrevrun/$folder" ]; then
         echo
         echo "Folder $folder not found"
-        sleep 0.25
+        sleep 0.5
         echo "Creating $folder"
         mkdir -v /opt/kevrevrun/$folder
         sleep 0.5
@@ -19,14 +19,13 @@ for folder in "cfg" "status" "scripts" "tmp" "logs"; do
         sleep 0.25
     fi
 done
-sleep 0.5
 echo
 echo "Checking file structure"
-sleep 0.5
+sleep 1
 for file in "/opt/kevrevrun/id.usr" "/opt/kevrevrun/name.usr" "/opt/kevrevrun/status/setup.stage" "/opt/kevrevrun/status/loop.status" "/opt/kevrevrun/install.dir"; do
     if [ ! -f $file ]; then
         echo
-        echo "Error: $file not found"
+        echo "The file - $file - was not found"
         if [ $file = "/opt/kevrevrun/id.usr" ]; then
             echo $UID > $file
             echo "Repaired $file"
@@ -47,14 +46,13 @@ for file in "/opt/kevrevrun/id.usr" "/opt/kevrevrun/name.usr" "/opt/kevrevrun/st
     else
         echo
         echo "File $file confirmed"
-        sleep 0.25
+        sleep 1
     fi
 done
-sleep 0.5
 # Creates a file that contains all folder used in the installation
 echo
 echo "Creating Setup Variables...Folders"
-sleep 0.5
+sleep 1
 cat << 'EOF' > /opt/kevrevrun/status/folders.list
 mainDir,/opt/kevrevrun
 statusDir,/opt/kevrevrun/status
@@ -64,25 +62,26 @@ tmpDir,/opt/kevrevrun/tmp
 logDir,/opt/kevrevrun/logs
 EOF
 echo
-echo "Setup variables saved to folders.list"
-sleep 0.5
+echo "Setup variables have been saved to folders.list"
+sleep 1
 # Sets the folder variables for each folder in folders.list
 echo
 echo "Setting up Folder Variables"
+sleep 1
 echo
-sleep 0.5
 fldrList=$(cat /opt/kevrevrun/status/folders.list)
 for f in $fldrList; do
     varName=$(echo $f | cut -d ',' -f 1)
     varValue=$(echo $f | cut -d ',' -f 2)
     export $varName="$varValue" 2>&1
     echo "Folder Variable $varName is set to $varValue"
-    sleep 0.25
+    sleep 0.5
 done
+sleep 1
 # Creates file that contains all files that hold a status across scripts and reboots.
 echo
 echo "Creating Setup Variables...Files"
-sleep 0.5
+sleep 1
 cat << 'EOF' > /opt/kevrevrun/status/files.list
 stgUsr,/opt/kevrevrun/id.usr
 stgUsrN,/opt/kevrevrun/name.usr
@@ -93,11 +92,11 @@ selDE,/opt/kevrevrun/status/selDE.status
 EOF
 echo
 echo "Setup variables saved to files.list"
-sleep 0.5
+sleep 1
 # Sets variables for the status files
 echo
 echo "Setting up File Variables"
-sleep 0.5
+sleep 1
 echo
 varFiles=$(cat /opt/kevrevrun/status/files.list)
 for v in $varFiles; do
@@ -105,13 +104,13 @@ for v in $varFiles; do
     varValue=$(echo $v | cut -d ',' -f 2)
     export $varName="$varValue"
     echo "File Variable $varName is set to $varValue"
-    sleep 0.25
+    sleep 0.5
 done
-sleep 0.5
+sleep 1
 # Creates a file that allows the saved variables to be called into the current script
 echo
 echo "Creating Setup Variables...Values"
-sleep 0.5
+sleep 1
 cat << 'EOF' > /opt/kevrevrun/status/values.list
 setupStage,/opt/kevrevrun/status/setup.stage
 usrId,/opt/kevrevrun/id.usr
@@ -120,19 +119,19 @@ setupDir,/opt/kevrevrun/setup.dir
 selDEValue,/opt/kevrevrun/status/selDE.status
 EOF
 echo
-echo "Setup variables saved to values.list"
-sleep 0.5
+echo "Setup variables have been saved to values.list"
+sleep 1
 # Reads the values from the files in values.list
-echo Confirming Setup Variables
 echo
+echo "Confirming Setup Variables"
 while IFS=',' read -r varName fileName ; do
     if [ -f $fileName ]; then
         echo
         echo "File $fileName confirmed"
     else
         echo
-        echo "Error: $fileName not found"
-        sleep 0.25
+        echo "The file - $fileName - was not found"
+        sleep 1
         echo "Creating missing file"
         sleep 0.5
         echo "0" > $fileName
@@ -140,9 +139,10 @@ while IFS=',' read -r varName fileName ; do
         sleep 0.5
 fi
 done < "/opt/kevrevrun/status/files.list"
+sleep 1
 echo
 echo "Reading and Exporting All Setup Variables"
-sleep 0.5
+sleep 1
 valueList=$(cat /opt/kevrevrun/status/values.list)
 for v in $valueList; do
     varName=$(echo $v | cut -d ',' -f 1)
@@ -152,6 +152,7 @@ for v in $valueList; do
     echo "Variable $varName has been imported with value $varValue"
     sleep 0.25
 done
+sleep 1
 echo 
 echo "Downloading Practical Wayland from github"
 sleep 1
@@ -159,40 +160,44 @@ echo
 git -C "$tmpDir" clone https://github.com/harbornode-ca/practical-wayland.git
 if [ -d "$tmpDir/practical-wayland" ]; then
     echo
-    echo "Practical Wayland directory confirmed"
-    sleep 0.5
+    echo "Practical Wayland sucessfully cloned"
+    sleep 1
 fi
 echo
 echo "Moving files from the installation directory to Main directory"
-sleep 0.5
+sleep 1
 echo
 #Move files from the installation directory to Main directory
 destFldr="$scriptDir $cfgDir"
 for f in $destFldr; do
     echo "Empting folder $f"
+    sleep 1
     rm -rvf $f/*
-    sleep 0.5
+    
 done
 echo
+sleep 1
 for f in $destFldr; do
     srcFldr=$(echo $f | cut -d '/' -f 4)
     echo "Copying files to folder $f"
+    sleep 0.5
     cp -Rv $tmpDir/practical-wayland/$srcFldr/* $f
     sleep 0.5
 done
 echo
 echo "Cleaning up temporary files"
-sleep 0.5
+sleep 1
 echo
 echo "Removing temporary extraction folder"
+sleep 0.5
 rm -rvf "$tmpDir/practical-wayland"
 sleep 0.5
 echo
 echo "Temporary files removed"
-sleep 0.5
+sleep 1
 echo 
 echo "Updating the stage file"
-sleep 0.5
+sleep 1
 echo "2" > $stageFile
 sleep 0.5
 echo "Stage file updated"
