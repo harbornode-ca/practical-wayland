69fe98d7745a3f557427f9b1f1074b845ee49a9f
Kevrevrun <kevin@harbornode.ca>
2026-09-18 15:10:44 -0400

    chore: rename GPU apt files, remove flatpak, and add ashell package lists

--- scripts/2609180913.sh
+++ scripts/2609180913.sh
@@ -68,7 +68,7 @@ else
     sleep 1.5
 fi
 if [ $installIntel == true ]; then
-    depIntel=$(cat $swAptDir/intelgpu.apt)
+    depIntel=$(cat $swAptDir/gpuIntel.apt)
     echo
     echo "Installing Intel GPU drivers"
     sleep 0.5
@@ -82,7 +82,7 @@ if [ $installIntel == true ]; then
     read -p "Press [ENTER] key to continue..."
 fi
 if [ $installAMD == true ]; then
-    depAMD=$(cat $swAptDir/amdgpu.apt)
+    depAMD=$(cat $swAptDir/gpuAMD.apt)
     echo
     echo "Installing AMD GPU drivers"
     sleep 0.5
@@ -96,8 +96,8 @@ if [ $installAMD == true ]; then
     read -p "Press [ENTER] key to continue..."
 fi
 if [ $installNVIDIA == true ]; then
-    depNVIDIA=$(cat $swAptDir/nvidia.apt)
-    urlNVIDIA=$(cat $swAptDir/nvidia.url)
+    depNVIDIA=$(cat $swAptDir/gpuNVIDIA.apt)
+    urlNVIDIA=$(cat $installDir/nvidia.url)
     echo
     echo "Adding Nvidia Driver Repository"
     sleep 0.5
