96391253927657195b73692ad5a170454d030e0b
Kevin Revelle <kevin@harbornode.ca>
2026-09-20 18:49:46 -0400

    refactor: update dev copy script to include data and tools directories and reduce sleep delays

--- tools/dev/cp-from-dev.sh
+++ tools/dev/cp-from-dev.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 #Moves files from the dev folder to the main folder
-destFldr="/opt/kevrevrun/scripts /opt/kevrevrun/cfg"
+destFldr="/opt/kevrevrun/scripts /opt/kevrevrun/cfg /opt/kevrevrun/data /opt/kevrevrun/tools"
 for f in $destFldr; do
-    echo "Emptying folder $F"
-    sleep 1
+    echo "Emptying folder $f"
+    sleep 0.25
     rm -rvf $f/*
-    sleep 1
+    sleep 0.25
 done
-echo Copying main directory contents
-cp -vf $HOME/practical-wayland/* /opt/kevrevrun
-sleep 1
 echo "Copying cfg folder contents"
 cp -rvf $HOME/practical-wayland/cfg/* /opt/kevrevrun/cfg
-sleep 1
+sleep 0.25
 echo "Copying scripts folder contents"
 cp -rvf $HOME/practical-wayland/scripts/* /opt/kevrevrun/scripts
-sleep 1
-
+sleep 0.25
+echo "Copying data folder contents"
+cp -rvf $HOME/practical-wayland/data/* /opt/kevrevrun/data
+sleep 0.25
+echo "Copying tools folder contents"
+cp -rvf $HOME/practical-wayland/tools/* /opt/kevrevrun/tools
+sleep 0.25
