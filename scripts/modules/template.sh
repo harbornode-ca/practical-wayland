9985d0956269e4c973348249a6fcd51ac9fecd5a
Kevin Revelle <kevin@harbornode.ca>
2026-09-21 16:37:52 -0400

    refactor: migrate reference files to notes directory and add templates

---
+++ scripts/modules/template.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#Script Information
+
+#START CMD FAIL FUNCTION
+cmdFail () {
+if [ $exitStat -ne 0 ]; then
+    style=lose
+    prt_info    
+    gum style "$errMsg"
+    sleep 1
+    echo
+    style=msg
+    prt_info
+    gum style "This script will now exit"
+    sleep 1
+    clear
+    exit 1
+else
+    style=win
+    prt_info
+    gum style "$successMsg"
+    sleep 0.5
+fi
+# These variables need to be set directly after a process ends to capture the $? value 
+# and output a message, cmdFail runs function.
+# exitStat=$?
+# errMsg="ERROR MESSAGE"
+# successMsg="SUCCESS MESSAGE"
+# cmdFail
+}
+#END CMD FAIL FUNCTION
+
+#START GUM VARIABLES
+#GUM CONFIRM VARIABLES
+export GUM_CONFIRM_PROMPT_FOREGROUND=7
+export GUM_CONFIRM_SELECTED_FOREGROUND=0
+export GUM_CONFIRM_SELECTED_BACKGROUND=2
+export GUM_CONFIRM_UNSELECTED_FOREGROUND=0
+export GUM_CONFIRM_UNSELECTED_BACKGROUND=3
+export GUM_CONFIRM_PADDING="2 0"
+export GUM_CONFIRM_SHOW_HELP=false
+#END GUM CONFIRM VARIABLES
+
+#START GUM CHOOSE VARIABLES
+export GUM_CHOOSE_PADDING="1 0"
+export GUM_CHOOSE_HEIGHT=10
+export GUM_CHOOSE_CURSOR=" > "
+export GUM_CHOOSE_CURSOR_PREFIX="[-] "
+export GUM_CHOOSE_SELECTED_PREFIX="[x] "
+export GUM_CHOOSE_UNSELECTED_PREFIX="[ ] "
+export GUM_CHOOSE_CURSOR_FOREGROUND=7
+export GUM_CHOOSE_HEADER_FOREGROUND=3
+export GUM_CHOOSE_ITEM_FOREGROUND=3
+export GUM_CHOOSE_SELECTED_FOREGROUND=10
+#END GUM CHOOSE VARIABLES
+
+#START GUM SPIN VARIABLES
+export GUM_SPIN_TITLE="Processing..."
+export GUM_SPIN_SPINNER_FOREGROUND=3
+export GUM_SPIN_TITLE_FOREGROUND=11
+export GUM_SPIN_SPINNER="dot"
+export GUM_SPIN_PADDING="1 0"
+#END GUM SPIN VARIABLES
+
+#START GUM STYLE FUNCTION
+prt_info (){
+case $style in
+    info) export FOREGROUND=7; export BOLD=true;;
+    msg) export FOREGROUND=3;;
+    lose) export FOREGROUND=1; export BOLD=true;;
+    win) export FOREGROUND=2; export BOLD=true;;
+    *) export FOREGROUND=7; export BOLD=true;;
+esac
+}
+#END GUM STYLE FUNCTION
+#END GUM VARIABLES
